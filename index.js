ba9782ec454e3e7305d4848fe115cc277bc489b9
Sergi Petit <sergi_hak_7@hotmail.com>
2021-11-03 18:22:13 +0100

    Added server , returning a table with the results

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 require("dotenv").config();
-
+const http = require("http");
 const printResults = require("./printResults");
+
+const server = http.createServer();
+
+const port = process.env.SERVER_CHALLENGE || 5000;
+
+server.listen(port);
+
+server.on("request", (request, response) => {
+  response.setHeader("Content-Type", "text/html");
+  response.end(printResults(1, 2));
+});
 
 const numbers = process.argv.slice(2).map((num) => parseInt(num, 10));
 
 const calculator = (a, b) => {
   if (!a && !b) {
-    console.log("asdg");
+    console.log("");
   } else if (!a || !b) {
     process.exit(0);
   } else {
@@ -14,4 +25,4 @@ const calculator = (a, b) => {
   }
 };
 
-calculator(numbers[0], numbers[1]);
+// calculator(numbers[0], numbers[1]);
